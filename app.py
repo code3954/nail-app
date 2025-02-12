8a16bedf4b09e35eac913b88489318452f9d5734
code3954 <namju978@gmail.com>
2025-02-12 13:20:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -13,14 +13,15 @@ def main():
     st.sidebar.title("네일 건강 확인해보기") 
 
     # 사이드바에서 라디오 버튼으로 메뉴 항목을 선택
-    menu = ["앱 소개", "네일상태 분석", "증상 예측하기"]
-    choice = st.sidebar.radio("메뉴", menu)
-    
-    if choice == menu[0]:
+    app_info = st.sidebar.checkbox("앱 소개")
+    nail_analysis = st.sidebar.checkbox("네일상태 분석")
+    symptom_prediction = st.sidebar.checkbox("증상 예측하기")
+
+    if app_info:
         show_home()
-    elif choice == menu[1]:
+    elif nail_analysis:
         show_eda()
-    elif choice == menu[2]:
+    elif symptom_prediction:
         show_ml()
         
 
